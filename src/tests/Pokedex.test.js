530257430aa2dea88acf2f93a1b8ca80a31a49e5
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-18 18:04:14 -0300

    Resolução do requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+import renderWithRouter from './utils/renderWithRouter';
+
+test('A página contém botões de seleção por tipo de pokémon', () => {
+  renderWithRouter(<App />);
+
+  const pokedexTitle = screen.getByRole('heading', { name: 'Encountered pokémons' });
+  expect(pokedexTitle).toBeInTheDocument();
+
+  const typeButtons = screen.getAllByTestId('pokemon-type-button');
+  const TYPE_BUTTONS_SIZE = 7;
+  expect(typeButtons).toHaveLength(TYPE_BUTTONS_SIZE);
+});
+
+test('A função chamada pelo botão "All" tem o argumento correto', () => {
+  renderWithRouter(<App />);
+
+  const allButton = screen.getByRole('button', { name: 'All' });
+  userEvent.click(allButton);
+
+  expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+});
+
+test('O botão dos tipos mostra o nome tipo no texto do botão', () => {
+  renderWithRouter(<App />);
+
+  const typeButtons = screen.getAllByTestId('pokemon-type-button');
+  typeButtons.forEach((button) => {
+    expect(button).not.toHaveTextContent('');
+  });
+});
