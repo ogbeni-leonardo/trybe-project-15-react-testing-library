a7d56aa91b9bdf9a7bd343edd66d87fea54647fb
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-18 16:59:01 -0300

    Resolução do requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+import renderWithRouter from './utils/renderWithRouter';
+
+test(
+  'A página de favoritos mostra a mensagem correta quando não há pokemons favoritos',
+  () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const paragraph = screen
+      .getByText('No favorite pokemon found');
+
+    expect(paragraph).toBeInTheDocument();
+  },
+);
