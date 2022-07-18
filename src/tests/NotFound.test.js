91eeab2780b529b8178f08ac99661ba5a7a5b7c8
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-07-18 17:08:03 -0300

    Resolução do requsito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+import renderWithRouter from './utils/renderWithRouter';
+
+test('Se a mensagem dá página está sendo exibida', () => {
+  renderWithRouter(<NotFound />);
+
+  const message = screen
+    .getByRole('heading', { name: 'Page requested not found Crying emoji' });
+
+  expect(message).toBeInTheDocument();
+});
+
+test('A imagem da página está sendo carregada', () => {
+  renderWithRouter(<NotFound />);
+
+  const alt = 'Pikachu crying because the page requested was not found';
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  const image = screen.getByAltText(alt);
+  expect(image.src).toBe(src);
+});
